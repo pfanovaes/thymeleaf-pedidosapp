9438513f07edef9f1496d719cedeef53f044a8ef
Patricia Novaes <patricia.novaes@gmail.com>
2022-03-09 21:00:26 -0300

    first commit

---
+++ src/main/java/br/com/letscode/PedidosAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.letscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PedidosAppApplication.class, args);
+    }
+
+}
