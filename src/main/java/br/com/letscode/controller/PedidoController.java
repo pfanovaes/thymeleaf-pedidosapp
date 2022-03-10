cf2d546950a59ddf713148b3cb4a1eaa3628d583
Patricia Novaes <patricia.novaes@gmail.com>
2022-03-10 20:12:37 -0300

    Aula 7 - Continuação projeto Thymeleaf

--- src/main/java/br/com/letscode/controller/PedidoController.java
+++ src/main/java/br/com/letscode/controller/PedidoController.java
@@ -5,8 +5,11 @@ import br.com.letscode.repository.PedidoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -21,4 +24,23 @@ public class PedidoController {
 
         return "listar-pedidos";
     }
+
+    @GetMapping("cadastrar-pedido-form")
+    public String cadastratPedidoForm() {
+        return "cadastrar-pedido-form";
+    }
+
+    @PostMapping("/cadastrar-pedido")
+        public RedirectView cadastrarPedido(PedidoEntidade entidade) {
+            PedidoRepository repository = new PedidoRepository();
+
+        LocalDate dataEntrega = LocalDate.now().plusDays(10);
+        entidade.setDataEntrega(dataEntrega);
+
+        repository.salvar(entidade);
+
+        final RedirectView view = new RedirectView("pedido/listar", true);
+        return view;
+    }
+
 }
--- src/main/java/br/com/letscode/repository/PedidoRepository.java
+++ src/main/java/br/com/letscode/repository/PedidoRepository.java
@@ -3,15 +3,32 @@ import br.com.letscode.entity.PedidoEntidade;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class PedidoRepository {
 
-    public List<PedidoEntidade> getAll() {
-        return Arrays.asList(new PedidoEntidade(1L, "Celular", BigDecimal.valueOf(799.0), LocalDate.now(), "iphone"),
-                new PedidoEntidade(2L, "Computador", BigDecimal.valueOf(1200.0), LocalDate.now(), "Positivo"),
-                new PedidoEntidade(3L, "Cadeira gamer", BigDecimal.valueOf(1000.0), LocalDate.now(), "razr"));
+    private static List<PedidoEntidade> pedidos = new ArrayList<>();
 
+    static {
+        pedidos.addAll(Arrays.asList(new PedidoEntidade(1L, "Celular", BigDecimal.valueOf(799.0), LocalDate.now(), "iphone"),
+                new PedidoEntidade(2L, "Computador", BigDecimal.valueOf(1200), LocalDate.now(), "Positivo"),
+                new PedidoEntidade(3L, "Computador", BigDecimal.valueOf(1200), LocalDate.now(), "ASUS"),
+                new PedidoEntidade(4L, "Computador", BigDecimal.valueOf(1200), LocalDate.now(), "IOS"),
+                new PedidoEntidade(5L, "Cadeira gamer", BigDecimal.valueOf(1000), LocalDate.now(), "razr")));
+    }
+
+    public PedidoEntidade salvar(PedidoEntidade entidade) {
+
+        entidade.setId(pedidos.get(pedidos.size() - 1).getId()+1);
+
+        pedidos.add(entidade);
+
+        return entidade;
+    }
+
+    public List<PedidoEntidade> getAll() {
+        return pedidos;
     }
 }
